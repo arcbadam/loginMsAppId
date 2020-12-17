9689433d40d0757f14e686e92560ab1c50924f9d
w2cluster583@iiht.tech <w2cluster583@iiht.tech>
2020-12-17 00:09:11 -0500

    first commit

---
+++ src/test/java/com/ibm/loginAppId/LoginMsAppIdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.loginAppId;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginMsAppIdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
